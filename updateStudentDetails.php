be53967f10b7b18e350d8c04c0c1851b476ef4c0
kevintran02 <happy65435@gmail.com>
2025-06-05 12:26:24 -0700

    finished update student details

--- updateStudentDetails.php
+++ updateStudentDetails.php
@@ -1,178 +1,105 @@
 <?php
-	session_start();	
-// Include config file
-	require_once "config.php";
+    session_start();	
+    // Include config file
+    require_once "config.php";
  
-// Define variables and initialize with empty values
-// Note: You can not update SSN 
-$f_name = $l_name = $number_of_classes = "";
-$f_name_err = $l_name_err = $number_of_classes_err = "" ;
-// Form default values
+    // Define variables and initialize with empty values
+    $f_name = $l_name = $email = "";
+    $f_name_err = $l_name_err = $email_err = $update_err = "";
 
-if(isset($_GET["student_id"]) && !empty(trim($_GET["student_id"]))){
-	$_SESSION["student_id"] = $_GET["student_id"];
+    if(isset($_GET["student_id"]) && !empty(trim($_GET["student_id"]))){
+        $_SESSION["student_id"] = $_GET["student_id"];
 
-    // Prepare a select statement
-    $sql1 = "SELECT * FROM Project_Student WHERE student_id = ?";
-  
-    if($stmt1 = mysqli_prepare($link, $sql1)){
-        // Bind variables to the prepared statement as parameters
-        mysqli_stmt_bind_param($stmt1, "s", $param_SID);      
-        // Set parameters
-       $param_SID = trim($_GET["student_id"]);
-
-        // Attempt to execute the prepared statement
-        if(mysqli_stmt_execute($stmt1)){
-            $result1 = mysqli_stmt_get_result($stmt1);
-			if(mysqli_num_rows($result1) > 0){
-
-				$row = mysqli_fetch_array($result1);
-
-                $l_name = $row['l_name'];
-                $f_name = $row['f_name'];
-                $number_of_classes = $row['number_of_classes'];
-			}
-		}
-	}
-}
- 
-// Post information about the student when the form is submitted
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    // the id is hidden and can not be changed
-    $student_id = $_SESSION["student_id"];
-    // Validate form data this is similar to the create Student file
-    // Validate name
-    $f_name = trim($_POST["f_name"]);
-    if(empty($f_name)){
-        $f_name_err = "Please enter a first name.";
-    } elseif(!filter_var($f_name, FILTER_VALIDATE_REGEXP, array("options=> array("regexp"=>"/^[a-zA-Z\s]+$/")))){
-        $f_name_err = "Please enter a valid first name.";
-    }
-
-    if(empty($Fname)){
-        $Fname_err = "Please enter a first name.";
-    } elseif(!filter_var($Fname, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
-        $Fname_err = "Please enter a valid first name.";
-    } 
-    $Lname = trim($_POST["Lname"]);
-    if(empty($Lname)){
-        $Lname_err = "Please enter a last name.";
-    } elseif(!filter_var($Lname, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
-        $Lname_err = "Please enter a valid last name.";
-    }  
-    // Validate Address
-    $Address = trim($_POST["Address"]);
-    if(empty($Address)){
-        $Address_err = "Please enter Address.";     
-    }
-	
-	// Validate Salary
-    $Salary = trim($_POST["Salary"]);
-    if(empty($Salary)){
-        $Salary_err = "Please enter salary.";    	
-	}
-	
-	// Validate Department Number
-    $Dno = trim($_POST["Dno"]);
-    if(empty($Dno)){
-        $Dno_err = "Please enter department number.";    	
-	}
-
-    // Check input errors before inserting into database
-    if(empty($Fname_err) && empty($Lname_err) && empty($Address_err) && empty($Salary_err) && empty($Dno_err)){
-        // Prepare an update statement
-        $sql = "UPDATE EMPLOYEE SET Fname=?, Lname=?, Address=?, Salary = ?, Dno = ? WHERE Ssn=?";
-    
-        if($stmt = mysqli_prepare($link, $sql)){
+        // Prepare a select statement
+        $sql1 = "SELECT * FROM Project_Student WHERE student_id = ?";
+      
+        if($stmt1 = mysqli_prepare($link, $sql1)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "sssdis", $param_Fname, $param_Lname,$param_Address, $param_Salary,$param_Dno, $param_Ssn);
-            
+            mysqli_stmt_bind_param($stmt1, "s", $param_SID);      
             // Set parameters
-            $param_Fname = $Fname;
-			$param_Lname = $Lname;            
-			$param_Address = $Address;
-            $param_Salary = $Salary;
-            $param_Dno = $Dno;
-            $param_Ssn = $Ssn;
-            
+            $param_SID = trim($_GET["student_id"]);
+
             // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Records updated successfully. Redirect to landing page
-                header("location: index.php");
-                exit();
-            } else{
-                echo "<center><h2>Error when updating</center></h2>";
+            if(mysqli_stmt_execute($stmt1)){
+                $result1 = mysqli_stmt_get_result($stmt1);
+                if(mysqli_num_rows($result1) > 0){
+                    $row = mysqli_fetch_array($result1);
+
+                    $l_name = $row['l_name'];
+                    $f_name = $row['f_name'];
+                    $email = $row['email'];
+                }
             }
-        }        
-        // Close statement
-        mysqli_stmt_close($stmt);
+        }
     }
-    
-    // Close connection
-    mysqli_close($link);
-} else {
 
-    // Check existence of sID parameter before processing further
-	// Form default values
+    // Processing form data when form is submitted
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $student_id = $_SESSION["student_id"];
 
-	if(isset($_GET["Ssn"]) && !empty(trim($_GET["Ssn"]))){
-		$_SESSION["Ssn"] = $_GET["Ssn"];
+        // Validate first name
+        $new_f_name = trim($_POST["f_name"]);
+        if(empty($new_f_name)){
+            $new_f_name = $f_name; // Retain old value if empty
+        } elseif(!filter_var($new_f_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+            $f_name_err = "Please enter a valid first name.";
+        }
 
-		// Prepare a select statement
-		$sql1 = "SELECT * FROM EMPLOYEE WHERE Ssn = ?";
-  
-		if($stmt1 = mysqli_prepare($link, $sql1)){
-			// Bind variables to the prepared statement as parameters
-			mysqli_stmt_bind_param($stmt1, "s", $param_Ssn);      
-			// Set parameters
-			$param_Ssn = trim($_GET["Ssn"]);
+        // Validate last name
+        $new_l_name = trim($_POST["l_name"]);
+        if(empty($new_l_name)){
+            $new_l_name = $l_name; // Retain old value if empty
+        } elseif(!filter_var($new_l_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+            $l_name_err = "Please enter a valid last name.";
+        }
 
-			// Attempt to execute the prepared statement
-			if(mysqli_stmt_execute($stmt1)){
-				$result1 = mysqli_stmt_get_result($stmt1);
-				if(mysqli_num_rows($result1) == 1){
+        // Validate email
+        $new_email = trim($_POST["email"]);
+        if(empty($new_email)){
+            $new_email = $email; // Retain old value if empty
+        } elseif(!filter_var($new_email, FILTER_VALIDATE_EMAIL)){
+            $email_err = "Please enter a valid email address.";
+        }
 
-					$row = mysqli_fetch_array($result1);
+        // Check if all values are the same
+        if ($new_f_name === $f_name && $new_l_name === $l_name && $new_email === $email) {
+            $update_err = "No changes were made. Please modify at least one field.";
+        }
 
-					$Lname = $row['Lname'];
-					$Fname = $row['Fname'];
-					$Salary = $row['Salary'];
-					$Bdate = $row['Bdate'];
-					$Address = $row['Address'];
-					$Sex = $row['Sex'];
-					$Dno = $row['Dno'];
-					$Super_ssn = $row['Super_ssn'];
-				} else{
-					// URL doesn't contain valid id. Redirect to error page
-					header("location: error.php");
-					exit();
-				}                
-			} else{
-				echo "Error in SSN while updating";
-			}		
-		}
-			// Close statement
-			mysqli_stmt_close($stmt1);
-        
-			// Close connection
-			mysqli_close($link);
-	}  else{
-        // URL doesn't contain id parameter. Redirect to error page
-        header("location: error.php");
-        exit();
-	}	
-}
+        // Check input errors before updating the database
+        if(empty($f_name_err) && empty($l_name_err) && empty($email_err) && empty($update_err)){
+            $sql = "UPDATE Project_Student SET f_name = ?, l_name = ?, email = ? WHERE student_id = ?";
+
+            if($stmt = mysqli_prepare($link, $sql)){
+                mysqli_stmt_bind_param($stmt, "ssss", $param_f_name, $param_l_name, $param_email, $param_SID);
+
+                $param_f_name = $new_f_name;
+                $param_l_name = $new_l_name;
+                $param_email = $new_email;
+                $param_SID = $student_id;
+
+                if(mysqli_stmt_execute($stmt)){
+                    header("location: index.php");
+                    exit();
+                } else {
+                    echo "Error updating record.";
+                }
+            }
+            mysqli_stmt_close($stmt);
+        }
+
+        mysqli_close($link);
+    }
 ?>
  
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Company DB</title>
+    <title>Update Student Record</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
-        .wrapper{
+        .wrapper {
             width: 500px;
             margin: 0 auto;
         }
@@ -184,36 +111,31 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <div class="row">
                 <div class="col-md-12">
                     <div class="page-header">
-                        <h3>Update Record for SSN =  <?php echo $_GET["Ssn"]; ?> </H3>
+                        <h3>Update Record for Student ID: <?php echo htmlspecialchars($_SESSION["student_id"]); ?></h3>
                     </div>
-                    <p>Please edit the input values and submit to update.
-                    <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post">
-						<div class="form-group <?php echo (!empty($Fname_err)) ? 'has-error' : ''; ?>">
-                            <label>First Name</label>
-                            <input type="text" name="Fname" class="form-control" value="<?php echo $Fname; ?>">
-                            <span class="help-block"><?php echo $Fname_err;?></span>
+                    <p>Please edit the input values and submit to update the record.</p>
+                    <?php 
+                    if (!empty($update_err)) {
+                        echo '<div class="alert alert-danger">' . $update_err . '</div>';
+                    }
+                    ?>
+                    <form action="<?php echo htmlspecialchars($_SERVER['REQUEST_URI']); ?>" method="post">
+                        <div class="form-group <?php echo (!empty($f_name_err)) ? 'has-error' : ''; ?>">
+                            <label for="f_name">First Name</label>
+                            <input type="text" name="f_name" id="f_name" class="form-control" value="<?php echo htmlspecialchars($f_name); ?>" placeholder="Enter first name">
+                            <span class="help-block"><?php echo $f_name_err; ?></span>
                         </div>
-						<div class="form-group <?php echo (!empty($Lname_err)) ? 'has-error' : ''; ?>">
-                            <label>Last Name</label>
-                            <input type="text" name="Lname" class="form-control" value="<?php echo $Lname; ?>">
-                            <span class="help-block"><?php echo $Lname_err;?></span>
+                        <div class="form-group <?php echo (!empty($l_name_err)) ? 'has-error' : ''; ?>">
+                            <label for="l_name">Last Name</label>
+                            <input type="text" name="l_name" id="l_name" class="form-control" value="<?php echo htmlspecialchars($l_name); ?>" placeholder="Enter last name">
+                            <span class="help-block"><?php echo $l_name_err; ?></span>
                         </div>
-						<div class="form-group <?php echo (!empty($Address_err)) ? 'has-error' : ''; ?>">
-                            <label>Address</label>
-                            <input type="text" name="Address" class="form-control" value="<?php echo $Address; ?>">
-                            <span class="help-block"><?php echo $Address_err;?></span>
+                        <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
+                            <label for="email">Email</label>
+                            <input type="email" name="email" id="email" class="form-control" value="<?php echo htmlspecialchars($email); ?>" placeholder="Enter email">
+                            <span class="help-block"><?php echo $email_err; ?></span>
                         </div>
-                        <div class="form-group <?php echo (!empty($Salary_err)) ? 'has-error' : ''; ?>">
-                            <label>Salary</label>
-                            <input type="text" name="Salary" class="form-control" value="<?php echo $Salary; ?>">
-                            <span class="help-block"><?php echo $Salary_err;?></span>
-                        </div>
-						<div class="form-group <?php echo (!empty($Dno_err)) ? 'has-error' : ''; ?>">
-                            <label>Department Number</label>
-                            <input type="number" min="1" max="20" name="Dno" class="form-control" value="<?php echo $Dno; ?>">
-                            <span class="help-block"><?php echo $Dno_err;?></span>
-                        </div>						
-                        <input type="hidden" name="Ssn" value="<?php echo $Ssn; ?>"/>
+                        <input type="hidden" name="student_id" value="<?php echo htmlspecialchars($_SESSION["student_id"]); ?>"/>
                         <input type="submit" class="btn btn-primary" value="Submit">
                         <a href="index.php" class="btn btn-default">Cancel</a>
                     </form>
