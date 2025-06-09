6d2b528525ff0645fc94b33f60de62378920a093
Joy Lim <limjoy@flip1.engr.oregonstate.edu>
2025-06-08 17:27:03 -0700

    nevemind, still applying styles to pages

--- dropClasses.php
+++ dropClasses.php
@@ -100,6 +100,7 @@ mysqli_close($link);
     <meta charset="UTF-8">
     <title>Drop Classes</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <link rel="stylesheet" href="css/dropClasses.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
     <style>
